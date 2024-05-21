4441a82687c55bd80392f81850356d782234b679
isaac G. S. Silva <isaacgabrielssz@gmail.com>
2024-05-21 20:33:42 -0300

    feat: Add rout Validation and Calculation
    Rota especifica para se entregar as informacoes de dimencoes e peso que filtra as traspostadoras que nao sao validas para o produto e calcula com base em funcao as que sao validas

---
+++ server/src/http/routs/calcule.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../../lib/prisma'
+import z from 'zod'
+import calc from '@/utils/formuleCalcFrete'
+
+export async function calcFrete(app: FastifyInstance) {
+  app.post('/calc/Frete', async (request, reply) => {
+    const trans = await prisma.frete.findMany()
+
+    const dataProdutc = z.object({
+      dimensao: z.object({
+        altura: z.number(),
+        largura: z.number()
+      }),
+      peso: z.number()
+    })
+
+    const { dimensao, peso } = dataProdutc.parse(request.body)
+
+    let options: Array<object> = []
+
+    trans.map(e => {
+      if (e.heigh_min < dimensao.altura && e.heigh_max > dimensao.altura) {
+        if (e.width_min < dimensao.largura && e.width_max > dimensao.largura) {
+          options.push({
+            nome: e.name_transport,
+            valor_frete: calc.formatNotR(peso, e.constant),
+            prazo_dias: e.delivery_time
+          })
+        }
+      }
+    })
+
+    return reply.status(200).send(options)
+  })
+}
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -2,11 +2,13 @@ import fastify from 'fastify'
 import { creatFrete } from './routs/creat-frete'
 import { getFrete } from './routs/get-frete'
 import { PORT } from '@/utils/PORT'
+import { calcFrete } from './routs/calcule'
 
 const app = fastify()
 
 app.register(creatFrete)
 app.register(getFrete)
+app.register(calcFrete)
 
 app
   .listen({
